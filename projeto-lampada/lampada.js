5d23afafa67740b4100e2936881f8870abd4e052
Viviane Coelho <111070369+VivianeCoel@users.noreply.github.com>
2023-06-16 15:01:47 -0300

    Update lampada.js

--- projeto-lampada/lampada.js
+++ projeto-lampada/lampada.js
@@ -2,24 +2,25 @@ const turnOn = document.getElementById( 'turnOn' );
 const turnOff = document.getElementById( 'turnOff' );
 const lamp = document.getElementById( 'lamp' );
 
-function isLampBroken() {
+function isLampBroken () {
     return lamp.src.indexOf('quebrada') > -1
 }
 
-function lampOn() {
+
+function lampOn () {
     if (!isLampBroken ()) {
-        lamp.src='./img/ligada.jpg';
+        lamp.src="./img/ligada.jpg";
     }
 }
+
 
 function lampOff() {
     if (!isLampBroken ()) {
-        lamp.src='./img/desligada.jpg';
+        lamp.src="./img/desligada.jpg";
     }
 }
-
-function lampBroken() {
-  lamp.src="./img/quebrada.jpg";
+function lampBroken () {
+    lamp.src="./img/quebrada.jpg";
 }
 turnOn.addEventListener ( 'click', lampOn );
 turnOff.addEventListener ( 'click', lampOff );
